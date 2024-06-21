3151d34a237c81f0a883ddfb26ed7e58de820c04
Dhareppa Halli <dharepps@gmail.com>
2024-06-21 12:54:09 +0530

    Changes For Implemented Toggle Cart And Added functionallity to remove Item from cart

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useValue } from "../itemContext";
 import styles from "../styles/CartModal.module.css";
 
 function CartModal() {
+  const { cart, clear, total, toggle } = useValue();
   return (
     <div className={styles.cartModal}>
       <div className={styles.closeButton} onClick={toggle}>
@@ -10,10 +12,20 @@ function CartModal() {
       <div className={styles.clearButton} onClick={clear}>
         Clear
       </div>
-      <div className={styles.itemContainer}></div>
+      <div className={styles.itemContainer}>
+        {cart.map((item) => {
+          return (
+            <div className={styles.cartCard} key={item.id}>
+              <h1>{item.name}</h1>
+              <h3>X {item.qty} </h3>
+              <h3>X {item.qty * item.price}</h3>
+            </div>
+          );
+        })}
+      </div>
       <div className={styles.total}>
         <div className={styles.totalText}>Total</div>
-        <div className={styles.totalPrice}>$Price</div>
+        <div className={styles.totalPrice}>{total}</div>
       </div>
     </div>
   );
--- src/itemContext.js
+++ src/itemContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useContext } from "react";
+import CartModal from "./components/CartModal";
 
 const itemContext = createContext();
 
@@ -10,29 +11,54 @@ function useValue() {
 function CustomItemContext({ children }) {
   const [total, setTotal] = useState(0);
   const [item, setItem] = useState(0);
+  const [showCart, setShowCart] = useState(false);
+  const [cart, setCart] = useState([]);
 
-  const handleAdd = (price) => {
-    setTotal(total + price);
+  const handleAdd = (prod) => {
+    const index = cart.findIndex((item) => item.id === prod.id);
+
+    if (index === -1) {
+      setCart([...cart, { ...prod, qty: 1 }]);
+      console.log(cart);
+      setTotal(total + prod.price);
+    } else {
+      cart[index].qty++;
+      setCart(cart);
+      console.log(cart);
+      setTotal(total + cart[index].price);
+    }
     setItem(item + 1);
   };
 
-  const handleRemove = (price) => {
-    if (total <= 0) {
-      return;
+  const handleRemove = (id) => {
+    const index = cart.findIndex((item) => item.id === id);
+
+    if (index !== -1) {
+      cart[index].qty--;
+      setItem(item - 1);
+      setTotal(total - cart[index].price);
+      if (cart[index].qty === 0) {
+        cart.splice(index, 1);
+      }
+      setCart(cart);
     }
-    setTotal((prevState) => prevState - price);
-    setItem(item - 1);
   };
 
   const clear = () => {
     setTotal(0);
     setItem(0);
+    setCart([]);
+  };
+
+  const toggle = () => {
+    setShowCart(!showCart);
   };
 
   return (
     <itemContext.Provider
-      value={{ total, item, handleAdd, handleRemove, clear }}
+      value={{ total, item, handleAdd, handleRemove, clear, toggle, cart }}
     >
+      {showCart && <CartModal toggle={toggle} />}
       {children}
     </itemContext.Provider>
   );
